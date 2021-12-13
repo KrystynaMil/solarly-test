746ec99d02db40e5fdf9fa2af91fbd86dc4f240a
KrystynaMil <kristina-manager@hotmail.com>
2021-12-13 03:56:30 +0100

    pushing all the files of test

---
+++ README.md
@@ -0,0 +1,47 @@
+# solarly-test
+
+## Table of contents
+
+- [solarly-test](#solarly-test)
+  - [Table of contents](#table-of-contents)
+  - [General info](#general-info)
+  - [Screenshots](#screenshots)
+  - [Technologies](#technologies)
+  - [Setup](#setup)
+  - [Status](#status)
+  - [Contact](#contact)
+
+## General info
+
+> Frontend Test Atelier Design
+
+## Screenshots
+
+![Example screenshot]()
+
+## Technologies
+
+- HTML
+- CSS
+- VSC code
+- JavaScript
+
+
+## Setup
+
+- `npm install`
+
+- `npm run start`
+
+- `npm run watch`
+
+
+
+## Status
+
+Project is: _done_
+
+
+
+## Contact
+
---
+++ js/script.js
@@ -0,0 +1,47 @@
+// navbar toogle 
+
+ const navToggler = document.querySelector(".nav-toggler");
+ navToggler.addEventListener("click", navToggle);
+
+ function navToggle() {
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+    const contactBtn = document.querySelector(".contact-btn");
+    nav.classList.toggle("open");
+    
+
+    if(nav.classList.contains("open")){
+    	nav.style.maxHeight = nav.scrollHeight + "px";
+       navToggler.innerHTML = 'Close'; 
+       contactBtn.style.left = "100px";
+       }
+     else{
+    	nav.removeAttribute("style");
+       navToggler.innerHTML = 'Menu';
+   contactBtn.removeAttribute("style");
+   
+   }
+    } ;
+
+   //creating li el
+
+ const app = document.querySelector('#list');
+
+ const langs = [''];
+ 
+ const nodes = langs.map(lang => {
+     const li = document.createElement('li');
+     const buttonFR = document.createElement("a");
+     buttonFR.setAttribute("class", "fr")
+     const buttonEN = document.createElement("a");
+     buttonEN.setAttribute("class", "en")
+     buttonEN.innerHTML = "EN"
+     buttonFR.innerHTML = "FR";
+     li.appendChild(buttonFR);
+     li.appendChild(buttonEN);
+     
+     return li;
+ });
+ 
+ app.prepend(...nodes);
+
